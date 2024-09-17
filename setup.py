dcdd3b5d09cd678a6711b27731ea846fd43b0777
Valay Dave <valaygaurang@gmail.com>
2024-09-17 08:58:09 -0700

    [packaging] fix setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,20 @@ from setuptools import setup, find_namespace_packages
 
 version = "0.0.1"
 
+
+def get_long_description() -> str:
+    with open("README.md") as fh:
+        return fh.read()
+
+
 setup(
     name="metaflow-checkpoint",
     version=version,
     description="An EXPERIMENTAL checkpoint decorator for Metaflow",
     author="Valay Dave",
     author_email="help@outerbounds.com",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
     packages=find_namespace_packages(include=["metaflow_extensions.*"]),
     py_modules=[
         "metaflow_extensions",
